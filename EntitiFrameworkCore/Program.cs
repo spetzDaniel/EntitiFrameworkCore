e83e82955bdf90af49ee5f084bbfa2ccf0e438fd
spetzDaniel <daniel.molasy@gmail.com>
2019-05-22 14:52:07 +0100

    Add project files.

---
+++ EntitiFrameworkCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EntitiFrameworkCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
